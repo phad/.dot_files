e4a8a04d11c25defd4d1cc82df27ea62ece83710
Paul Hadfield <paul@songkick.com>
2013-03-26 09:46:33 +0000

    Remove echo statements now I know it works as intended

--- bashprofile.sh
+++ bashprofile.sh
@@ -5,13 +5,10 @@ src="$HOME/.dot_files/bashprofile.d"
 for dir in `ls "$src"`
 do
   if [ -d "$src/$dir" ]; then
-    echo "* "$dir
     for file in `ls "$src/$dir"`
     do
       if [ -f "$src/$dir/$file" ]; then
-        echo " + "$file
         . "$src/$dir/$file"
-        echo "   "$file" (done)"
       fi
     done
   fi
