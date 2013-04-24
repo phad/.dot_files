207336faf52092b1a7ed4e8d3f3efbd7ad511c97
Paul Hadfield <paul@songkick.com>
2013-04-24 11:26:22 +0100

    RVM: use 1.9.3 by default.  Add aliases for hadoop.

--- bashprofile.d/02_rvm/00_load_rvm.sh
+++ bashprofile.d/02_rvm/00_load_rvm.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 # Load RVM into a shell session *as a function*
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" 
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
+rvm use 1.9.3
+
---
+++ bashrc.d/05_hadoop/00_aliases.sh
@@ -0,0 +1,5 @@
+# from http://cloudblog.8kmiles.com/2012/01/09/hadoop-autocomplete-for-hadoop-dfs-commands/
+alias hdfs="hadoop dfs"
+# Note: you can make different alias for different hdfs. e.g. I use the following alias for remote cluster.
+# TODO(paul): understand this better before using it
+#alias hdfscluster="hadoop dfs -fs dfs://ubuntu-server:10418/"
